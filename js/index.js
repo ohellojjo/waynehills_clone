86bdd84feeeefe52dcbfe6f049484357191da64c
ohellojjo <ohellojjo@gmail.com>
2021-09-08 15:40:48 +0900

    JS part 구성

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function popup_open(){
+    document.getElementById("popup").style.display="block"
+}
+document.querySelector("#popBtn").addEventListener("click",popup_open);
+
+function popup_close(){
+    document.getElementById("popup").style.display="none"
+}
+document.querySelector("#closeBtn").addEventListener("click",popup_close);
+document.querySelector("#popback").addEventListener("click",popup_close);
+
+// document.querySelector("#popupBox").addEventListener('click', function(){
+//     document.querySelector("#popup").removeEventListener("click",popup_close);
+//   });
+  
+function signIn(){
+    location.href="./login/"
+}
