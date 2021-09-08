4540389675d264207a07dbe34adc40b6f5f95d4d
ohellojjo <ohellojjo@gmail.com>
2021-09-09 03:49:21 +0900

    코드수정 및 업데이트

--- js/index.js
+++ js/index.js
@@ -16,17 +16,27 @@ let mainpages = new Vue({
         popBtn(){
             this.popups=true;
         },
-        text1(){
+        text1(n){ 
+            console.log(n)
             document.querySelector("#slideText").children[0].style.opacity=1;
             document.getElementById("slideImages").style.left="0px";
+            document.querySelector("#slideText").children[0].style.borderBottom="3px solid #c7b9b4";
+            document.querySelector("#slideText").children[2].style.borderBottom="0";
+            document.querySelector("#slideText").children[1].style.borderBottom="0";
         },
         text2(){
             document.querySelector("#slideText").children[1].style.opacity=1;
             document.getElementById("slideImages").style.left="-480px";
+            document.querySelector("#slideText").children[1].style.borderBottom="3px solid #c7b9b4";
+            document.querySelector("#slideText").children[0].style.borderBottom="0";
+            document.querySelector("#slideText").children[2].style.borderBottom="0";
         },
         text3(){
             document.querySelector("#slideText").children[2].style.opacity=1;
             document.getElementById("slideImages").style.left="-960px";
+            document.querySelector("#slideText").children[2].style.borderBottom="3px solid #c7b9b4";
+            document.querySelector("#slideText").children[0].style.borderBottom="0";
+            document.querySelector("#slideText").children[1].style.borderBottom="0";
         }
     },
     components:{
@@ -35,9 +45,11 @@ let mainpages = new Vue({
     computed:{
         slide(){
             let ea = document.getElementById("slideImages").children.length;
-            
             let timer = setInterval(function(){
                 document.getElementById("slideImages").style.left=-(480*no)+"px"; 
+                document.querySelector("#slideText").children[0].style.borderBottom="unset";
+                document.querySelector("#slideText").children[1].style.borderBottom="unset";
+                document.querySelector("#slideText").children[2].style.borderBottom="unset";
                 document.querySelector("#slideText").children[no].style.borderBottom="3px solid #c7b9b4";
                 no++; 
                 if(no>=ea){
