45f1b04e8c17476639805ed5b1926b1f26ea73fa
ohellojjo <ohellojjo@gmail.com>
2021-09-08 23:20:38 +0900

    중간 수정중 재 수정 예정

--- js/index.js
+++ js/index.js
@@ -1,18 +1,40 @@
-function popup_open(){
-    document.getElementById("popup").style.display="block"
-}
-document.querySelector("#popBtn").addEventListener("click",popup_open);
+// function popup_open(){
+//     document.getElementById("popup").style.display="block"
+// }
+// document.querySelector("#popBtn").addEventListener("click",popup_open);
 
-function popup_close(){
-    document.getElementById("popup").style.display="none"
-}
-document.querySelector("#closeBtn").addEventListener("click",popup_close);
-document.querySelector("#popback").addEventListener("click",popup_close);
+// function popup_close(){
+//     document.getElementById("popup").style.display="none"
+// }
+// document.querySelector("#closeBtn").addEventListener("click",popup_close);
+// document.querySelector("#popback").addEventListener("click",popup_close);
 
-// document.querySelector("#popupBox").addEventListener('click', function(){
-//     document.querySelector("#popup").removeEventListener("click",popup_close);
-//   });
-  
-function signIn(){
-    location.href="./login/index.html"
-}
+// // document.querySelector("#popupBox").addEventListener('click', function(){
+// //     document.querySelector("#popup").removeEventListener("click",popup_close);
+// //   });
+
+
+let mainpages = new Vue({
+    el:"#mainP",
+    data:{
+        popups:false,
+    },
+    methods:{
+        signIn(){
+            location.href="./login/index.html"
+        },
+        closeBtn(){
+            this.popups=false;
+        },
+        popBtn(){
+            this.popups=true;
+        }
+
+    },
+    components:{
+
+    },
+    computed:{
+
+    }
+})
